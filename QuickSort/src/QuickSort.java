fd1c246ca914c940be47ab325e3dde262a994a9e
Koen <Koen@KOEN-LAPTOP>
2017-03-14 15:29:33 +0100

    Added QuickSort snippet.

---
+++ QuickSort/src/QuickSort.java
@@ -0,0 +1,14 @@
+/**
+ *
+ * @author Koen
+ */
+public class QuickSort {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
